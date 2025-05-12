f5824961667a1a0fe4758d07368ff6eef944938d
Rahulvmrdk <135455794+Rahulvmrdk@users.noreply.github.com>
2025-05-12 20:54:11 +0530

    Update speech_recognition.py

--- preprocessing/speech_recognition.py
+++ preprocessing/speech_recognition.py
@@ -1,8 +1,10 @@
 # preprocessing/speech_recognition.py
 
 import whisper
+import os
 
 def transcribe_audio(audio_path):
+    
     model = whisper.load_model("base")  # Use "small" or "medium" for better accuracy
     result = model.transcribe(audio_path)
     return result["text"]
